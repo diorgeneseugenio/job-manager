f56dd68339dfbf8ab29d4c1efc2295af249fdfc4
diorgeneseugenio <diorgeneseugenio@gmail.com>
2021-03-29 10:53:39 -0300

    Add jobs return in queries of Company

--- job-manager-api-gateway/src/graphql/typeDefs.js
+++ job-manager-api-gateway/src/graphql/typeDefs.js
@@ -2,12 +2,25 @@ import { gql } from "apollo-server";
 
 const typeDefs = gql`
   type Company {
+    id: ID!
     logo: String
     name: String!
     aboutDescription: String!
     city: String!
     state: String!
+    jobs: [Job]
+  }
+
+  type Job {
     id: ID!
+    companyId: ID
+    title: String!
+    description: String!
+    salary: Float!
+    benefits: String
+    skills: String
+    allowRemote: Boolean!
+    ended: Boolean!
   }
 
   type Mutation {
